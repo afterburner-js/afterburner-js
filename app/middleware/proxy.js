15de0e6cc7cdf730fdf52e5f627338f91eef7d90
ctcpip <ctcpip@users.noreply.github.com>
2023-05-26 16:10:02 -0500

    🔧 refactor proxy configuration

--- app/middleware/proxy.js
+++ app/middleware/proxy.js
@@ -1,5 +1,12 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
+const customProxySettings = {
+  allowCrossOrigin: false,
+  disableBrowserLoginPrompt: false,
+  disableSecureCookies: false,
+  ignoreECONNRESET: false,
+};
+
 module.exports = proxyTarget => {
   return createProxyMiddleware(pathname => {
     // TODO: document this
@@ -19,23 +26,79 @@ module.exports = proxyTarget => {
     protocolRewrite: 'http',
     secure: false,
     logLevel: 'warn',
-    onProxyRes: proxyRes => {
+    onProxyRes: (proxyRes, req) => {
 
-      // prevents browser login prompt from appearing when we get a 401 response
-      delete proxyRes.headers['www-authenticate'];
+      if (customProxySettings.disableBrowserLoginPrompt) {
+        // prevents browser login prompt from appearing when we get a 401 response
+        delete proxyRes.headers['www-authenticate'];
+      }
 
-      const cookie = proxyRes.headers['set-cookie'];
+      if (customProxySettings.disableSecureCookies) {
+        disableSecureCookies(proxyRes);
+      }
 
-      if (Array.isArray(cookie)) {
-
-        proxyRes.headers['set-cookie'] = cookie.map(c => {
-          return c.split(';')
-            .filter(v => { return v.trim().toLowerCase() !== 'secure'; })
-            .join('; ');
-        });
-
+      if (customProxySettings.allowCrossOrigin) {
+        allowCrossOrigin(req, proxyRes);
       }
 
     },
+    logProvider: logProvider => {
+
+      logProvider.error = function(e) {
+
+        if (customProxySettings.ignoreECONNRESET) {
+          ignoreECONNRESET(e);
+        }
+        else {
+          console.error(e);
+        }
+
+      };
+
+      return logProvider;
+
+    }
   });
 };
+
+function allowCrossOrigin(req, proxyRes) {
+
+  if (req.method === 'OPTIONS') {
+    proxyRes.statusCode = 200;
+  }
+
+  // ALL THE CORS
+  proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+  proxyRes.headers['Access-Control-Allow-Headers'] = '*';
+  proxyRes.headers['Access-Control-Allow-Methods'] = '*';
+
+}
+
+function disableSecureCookies(proxyRes) {
+
+  const cookie = proxyRes.headers['set-cookie'];
+
+  if (Array.isArray(cookie)) {
+
+    proxyRes.headers['set-cookie'] = cookie.map(c => {
+      return c.split(';')
+        .filter(v => { return v.trim().toLowerCase() !== 'secure'; })
+        .join('; ');
+    });
+
+  }
+
+}
+
+function ignoreECONNRESET(e) {
+
+  // we get these ECONNRESET errors in the log when
+  // the test navigates away from a page before an
+  // AJAX/fetch request completes.  we don't care so,
+  // we don't log it:
+
+  if (!e.endsWith('[ECONNRESET] (https://nodejs.org/api/errors.html#errors_common_system_errors)')) {
+    console.error(e);
+  }
+
+}
